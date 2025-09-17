a90faf69fde1f4e42e7179de949f220fe618ca3c
김재훈 <temp7045@gmail.com>
2025-09-18 00:27:23 +0900

    feat: add image list export

---
+++ src/assets/images/minions/index.tsx
@@ -0,0 +1,9 @@
+export const minions = [
+  require("./minions1.jpg"),
+  require("./minions2.jpg"),
+  require("./minions3.jpg"),
+  require("./minions4.jpg"),
+  require("./minions5.jpg"),
+  require("./minions6.jpg"),
+  require("./minions7.jpg"),
+];
